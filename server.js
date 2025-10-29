ed69702946cc3ea7e27aadf54b4b722e107bb423
ankandhang <ankandhang@gmail.com>
2025-10-29 12:22:10 +0530

    Updated server.js  with new changes

--- server.js
+++ server.js
@@ -2,15 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 5000;
-
-// Serve static files from public folder
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve index.html on root URL
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
